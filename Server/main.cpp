f83cacc8f76d85c403cf0dfbc655a04b839904ae
JustSalty <32201651+JustSalty@users.noreply.github.com>
2017-12-03 14:50:05 +0200

    Add files via upload

---
+++ Server/main.cpp
@@ -0,0 +1,15 @@
+#include <WinSock2.h>
+#include "C:\Users\Admin\Desktop\Chat2\SockUtils.h"
+#include <iostream>
+
+void main()
+{
+	try
+	{
+		InitSocketsLib();
+		TestServerSocket();
+
+		ReleaseSocketsLib();
+	}
+	catch (...) { cout << "\n Unidentified exception caught!\n"; }
+}
